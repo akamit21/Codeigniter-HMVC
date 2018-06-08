87c8482f8055409780f413e4d0e158e714b7690c
Amit Kumar <akamit21@gmail.com>
2018-06-09 02:29:32 +0530

    Add files via upload

---
+++ application/config/google.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+| -------------------------------------------------------------------
+|  Google API Configuration
+| -------------------------------------------------------------------
+|  client_id         string   Your Google API Client ID.
+|  client_secret     string   Your Google API Client secret.
+|  redirect_uri      string   URL to redirect back to after login.
+|  application_name  string   Your Google application name.
+|  api_key           string   Developer key.
+|  scopes            string   Specify scopes
+*/
+
+$config['google']['client_id']        = '676094313024-411q6u66uu8lhv3gvkgqdrflrbl6gpd3.apps.googleusercontent.com';
+$config['google']['client_secret']    = 'vGzNiuthcyRn9qUZGDsZyhBk';
+$config['google']['redirect_uri']     = 'http://localhost/new-admin/';
+$config['google']['application_name'] = 'Admin';
+$config['google']['api_key']          = '';
+$config['google']['scopes']           = array();
